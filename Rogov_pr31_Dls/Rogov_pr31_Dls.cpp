9df747f0cfd985d816495b3cf0fb51acfee3e390
AiromanR <110393090+AiromanR@users.noreply.github.com>
2025-10-21 22:56:09 +0500

    Update Args Rogov_pr31_Dls.cpp

--- Rogov_pr31_Dls/Rogov_pr31_Dls.cpp
+++ Rogov_pr31_Dls/Rogov_pr31_Dls.cpp
@@ -1,22 +1,34 @@
-﻿// Rogov_pr31_Dls.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+// Rogov_pr31_Dls.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
 
 struct EventSystem {
     struct Node {
-        void(*data)();
+
+        union Handlers{
+            void(*simpleHandler)();
+            void(*messageHandler)(std::string*);
+        };
+        Handlers Handler;
+
+        std::string* messages;
         Node* next;
 
-        Node(void(*value)()) : data(value), next(nullptr) {};
+        Node(void(*handler)()) :  messages(nullptr), next(nullptr) {
+            Handler.simpleHandler = handler;
+        }
+        Node(void(*handler)(std::string*), std::string* msgs) :  messages(msgs), next(nullptr) {
+            Handler.messageHandler = handler;
+        }
     };
 
     Node* head = nullptr;
     Node* tail = nullptr;
 
 public:
-    void registerHandler(EventSystem& system, void(*handler)()) {
-        Node* node = new Node(*handler);
+    void registerHandler(void(*handler)()) {
+        Node* node = new Node(handler);
 
         if (head == nullptr) {
             head = node;
@@ -28,36 +40,76 @@ public:
         }
         tail = node;
     }
-    void triggerEvent(const EventSystem& system) {
+
+    void registerHandler(void(*handler)(std::string*), std::string* messages) {
+        Node* node = new Node(handler, messages);
+
+        if (head == nullptr) {
+            head = node;
+            tail = node;
+        }
+        else {
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    void triggerEvent() {
         for (Node* i = head; i != nullptr; i = i->next) {
-            i->data();
+            if (i->messages) {
+                i->Handler.messageHandler(i->messages);
+            }
+            else {
+                i->Handler.simpleHandler();
+            }
         }
     }
 };
-void handler(std::string message) {
-    std::cout << message << std::endl;
-}
+
 void onUserLogin() {
     std::cout << "Пользователь вошел в систему\n";
 }
+
 void onUserLogout() {
     std::cout << "Пользователь вышел из системы\n";
 }
+
 void onError() {
     std::cout << "Произошла ошибка\n";
 }
+
+void onError(std::string* messages) {
+    std::cout << "Произошла ошибка:";
+    for (int i = 0; messages[i] != ""; i++) {
+        if (i != 0) std::cout << ", \"" << messages[i] << "\"";
+        else std::cout << " \"" << messages[i] << "\"";
+    }
+    std::cout << std::endl;
+}
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
     EventSystem system;
 
-    system.registerHandler(system, &onUserLogin);
-    system.registerHandler(system, &onUserLogout);
-    system.registerHandler(system, &onUserLogout);
-    system.registerHandler(system, &onError);
-    system.registerHandler(system, &onError);
-    system.registerHandler(system, &onError);
-    system.registerHandler(system, &onError);
+    system.registerHandler(&onUserLogin);
+    system.registerHandler(&onUserLogout);
+    system.registerHandler(&onUserLogout);
+    system.registerHandler(&onError);
 
-    system.triggerEvent(system);
+
+    std::string error1[] = { "Неверный пароль", ""};
+    system.registerHandler(&onError, error1);
+
+    std::string error2[] = { "Соединение разорвано", "Таймаут", "" };
+    system.registerHandler(&onError, error2);
+
+    std::string error3[] = { "Неверный пароль", "Двааааааааа", "Триииииииииии", ""};
+    system.registerHandler(&onError, error3);
+
+    std::string error4[] = { "11111111111111", "222222222222", "33333333333333", "444444444444" , ""};
+    system.registerHandler(&onError, error4);
+
+    system.triggerEvent();
 }
+
